f049a23e9922f5890b93343e9d5f272859f931a1
Mohamed Alaa <mohamed.alaa@gfi.world>
2021-06-03 15:39:49 +0200

    Initi Project

---
+++ CartDemo/Context/CartDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CartDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CartDemo.Context
+{
+    public class CartDbContext : DbContext
+    {
+        public CartDbContext(DbContextOptions<CartDbContext> contextOptions):base(contextOptions)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
